fcabd1a5e921ff2dc996a97d3d9d0e42f22e478d
Alejandro Pacheco <ale_pach03@hotmail.com>
2023-06-03 12:25:10 -0700

    Modified about page to include links and more content

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ window.onscroll = function() {
         header.classList.remove('navbarScroll');
     }
 }
+
+
+var typed = new Typed(".auto-type", {
+    strings:["Alejandro Pacheco", "a student"],
+    typeSpeed: 75,
+    backSpeed: 75,
+    loop: true
+});
